4d188c799615ce40a8f5c21d762263b603cf1824
Diana Rico <dianarico@itm.edu.co>
2023-04-29 00:47:52 -0500

    He terminado la clase usuario

--- apibiblio/src/main/java/apibiblio/apibiblio/modelos/usuario.java
+++ apibiblio/src/main/java/apibiblio/apibiblio/modelos/usuario.java
@@ -1,5 +1,43 @@
 package apibiblio.apibiblio.modelos;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+
 public class usuario {
+    @Id
+    @Column(name ="idusuario")
+    private long idlusuario;
+    
+   @Column(name = "carnet", length = 100)
+    private String carnet; 
+
+    @Column(name = "nombre", length = 100)
+    private String nombre; 
+    @Column(name = "apellido", length = 100)
+    private String apellido;
+    public long getIdlusuario() {
+        return idlusuario;
+    }
+    public String getCarnet() {
+        return carnet;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setIdlusuario(long idlusuario) {
+        this.idlusuario = idlusuario;
+    }
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    } 
     
 }
